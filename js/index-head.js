587d8c830d144005711d86b53f1be667d030c2ab
王鑫 <wangxin@wangxindeMacBook-Air.local>
2021-12-15 19:36:21 +0800

    cookie

--- js/index-head.js
+++ js/index-head.js
@@ -13,17 +13,14 @@ function showTime() {
 
 
 // 获取指定名称的cookie
-function getCookie(name) {
-  var strcookie = document.cookie; //获取cookie字符串
-  var arrcookie = strcookie.split("; "); //分割
-  //遍历匹配
-  for (var i = 0; i < arrcookie.length; i++) {
-    var arr = arrcookie[i].split("=");
-    if (arr[0] == name) {
-      return arr[0];
-    }
-  }
-  return "";
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+	}
+	return "";
 }
 
 var uid = getCookie("uid");
@@ -36,5 +33,5 @@ if (c_start == -1) {
 } else {
   document.getElementById("user-a").style.visibility="hidden";
   document.getElementById("user-p").style.visibility="visible";
-  document.getElementById("user-p").innerHTML=uid;
+  document.getElementById("user-p").innerHTML="你好："+uid;
 }
--- js/login.js
+++ js/login.js
@@ -4,17 +4,14 @@ var login_b = document.querySelector("#login_b");
 var register_b = document.querySelector("#register_b");
 
 // 获取指定名称的cookie
-function getCookie(name) {
-  var strcookie = document.cookie; //获取cookie字符串
-  var arrcookie = strcookie.split("; "); //分割
-  //遍历匹配
-  for (var i = 0; i < arrcookie.length; i++) {
-    var arr = arrcookie[i].split("=");
-    if (arr[0] == name) {
-      return arr[0];
-    }
-  }
-  return "";
+function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+	}
+	return "";
 }
 
 var uid = getCookie("uid");
@@ -27,9 +24,15 @@ if (c_start == -1) {
   window.location.href = "./index.html";
 }
 
-if ((uid = "")) {
-  window.alert(uid);
+
+
+function setCookie(cname,cvalue,exdays){
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname+"="+cvalue+"; "+expires;
 }
+
 login_b.onclick = function Login() {
   lo = new XMLHttpRequest();
   lo.onload = function () {
@@ -38,7 +41,7 @@ login_b.onclick = function Login() {
     //var r = eval("(" + a + ")")
     window.alert(d.message);
     if (d.code == 200) {
-      document.cookie="uid="+username.value;
+      setCookie("uid",username.value,300);
       window.location.href = "../index.html";
     }
   };
